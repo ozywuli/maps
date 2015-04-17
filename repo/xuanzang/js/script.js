496e12d1bf620b00b98bbfab55c048ff982e7815
ozywuli <ozy.wu.li@gmail.com>
2015-04-16 23:02:47 -0700

    trying to load geojson externally

--- repo/xuanzang/js/script.js
+++ repo/xuanzang/js/script.js
@@ -1,28 +1,14 @@
+$('document').ready(function() {
+
+
 L.mapbox.accessToken = 'pk.eyJ1IjoiYW9zaWthIiwiYSI6IjQzRGIxeEkifQ.7OvmyBbXwwt9Qxjlh9Qd3w';
 // In this case, we just hardcode data into the file. This could be dynamic.
 // The important part about this data is that the 'id' property matches
 // the HTML above - that's how we figure out how to link up the
 // map and the data.
-var places = { type: 'FeatureCollection', features: [
-{ geometry: { type: "Point", coordinates: [-0.12960000, 51.50110000] },
-  properties: { id: "cover", zoom: 9 }, type: 'Feature' },
-{ geometry: { type: "Point", coordinates: [-0.15591514, 51.51830379] },
-  properties: { id: "baker" }, type: 'Feature' },
-{ geometry: { type: "Point", coordinates: [-0.07571203, 51.51424049] },
-  properties: { id: "aldgate" }, type: 'Feature' },
-{ geometry: { type: "Point", coordinates: [-0.08533793, 51.50438536] },
-  properties: { id: "london-bridge" }, type: 'Feature' },
-{ geometry: { type: "Point", coordinates: [0.05991101, 51.48752939] },
-  properties: { id: "woolwich" }, type: 'Feature' },
-{ geometry: { type: "Point", coordinates: [-0.18335806, 51.49439521] },
-  properties: { id: "gloucester" }, type: 'Feature' },
-{ geometry: { type: "Point", coordinates: [-0.19684993, 51.5033856] },
-  properties: { id: "caulfield-gardens" }, type: 'Feature' },
-{ geometry: { type: "Point", coordinates: [-0.10669358, 51.51433123] },
-  properties: { id: "telegraph" }, type: 'Feature' },
-{ geometry: { type: "Point", coordinates: [-0.12416858, 51.50779757] },
-  properties: { id: "charing-cross" }, type: 'Feature' }
-]};
+
+
+
 
 
 
@@ -30,14 +16,12 @@ var places = { type: 'FeatureCollection', features: [
 // initiate map with options
 var map = L.mapbox.map('map', 'examples.map-i86nkdio', {
     zoomControl: true
-});
+}).setView([51.50110000, -0.12960000], 9);
 
 // disable zoom wheel
 map.scrollWheelZoom.disable();
 
-// add markers to map
-var placesLayer = L.mapbox.featureLayer(places)
-    .addTo(map);
+
 
 
 // declare all variables at top
@@ -55,8 +39,26 @@ currentId = '';
 
 
 
+
+
+// add markers to map
+var placesLayer = L.mapbox.featureLayer()
+    .loadURL('data/data.geojson')
+    .addTo(map);
+
+
+console.log(placesLayer);
+
+
+
 // highlight marker and corresponding panel on click
+
+$('document').ready(function() {
+
+
 placesLayer.eachLayer(function(layer) {
+
+console.log(1);
 
     layer.on('click', function() {
         setId(this.feature.properties.id);
@@ -64,20 +66,25 @@ placesLayer.eachLayer(function(layer) {
         var $listItem = $('.active');
 
 
-
         $('html, body').animate({
-            scrollTop: $listItem.offset().top - $('html, body').offset().top + $('html, body').scrollTop()
+            scrollTop: $listItem.offset().top - 100
         });
 
-        console.log($listItem.offset().top);
-        console.log($listItem.scrollTop());
-        console.log($('html, body').offset().top);
-        console.log($('html, body').scrollTop());
+
 
 
     })
 
+
+
+
 });
+
+});
+
+
+
+
 
 
 
@@ -135,36 +142,57 @@ function setId(newId) {
 
 
 
-console.log( $($sections) );
 
-console.log($sections[2].id);
+
 
 
 // If you were to do this for real, you would want to use
 // something like underscore's _.debounce function to prevent this
 // call from firing constantly.
-$(window).scroll(function() {
+$(window).on('scroll', function() {
 
-
+setTimeout(function(){
     var narrativeHeight = $narrative.height();
     var newId = currentId;
+
+
 
     // Find the section that's currently scrolled-to.
     // We iterate backwards here so that we find the topmost one.
     for (var i = $sections.length - 1; i >= 0; i--) {
 
+        var sections = $($sections[i]);
+        var rect = sections.offset();
 
-        var rect = $($sections[i]).offset().top;
 
-        if ( rect >= $('html, body').scrollTop() ) {
+
+        if ( rect.top >= $('html, body').scrollTop() - 100 ) {
             newId = $sections[i].id;
         }
-
 
 
 
     };
 
     setId(newId);
-
+ }, 500);
 }); // end window scroll
+
+
+
+
+
+
+}); // end document ready
+
+
+
+
+
+
+
+
+
+
+
+
